ec4cfa65e8d8876525dcee25298e2adf747a3849
Yash Codezzzz 24 <yashcodezzzz24@gmail.com>
2026-01-20 23:37:28 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://swish-frontend.onrender.com',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
